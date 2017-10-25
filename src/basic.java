ae2bfb79011134386b9f613826646bab28ad448a
Dakota <RoboRoyal@users.noreply.github.com>
2017-10-25 19:49:43 -0400

    Update basic.java

--- src/basic.java
+++ src/basic.java
@@ -2,20 +2,28 @@ package robosub;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.BasicConfigurator;
-import java.util.Scanner;
 
+/**
+ * Holds the most basic functions but mostly helps initiate and shutdown
+ * Hands off control to CORE after init and start
+ * @author Dakota
+ *
+ */
 @SuppressWarnings("unused")
 public class basic {
+	public static String[] mode_names = {"0","0","sonar nav","debug","test move","test move 2","dummy multi","0","dummy","0","dummy 2","0","compitition sonar nav",};
 	private static final int SONAR_NAV = 2;// normal sonar navigation
 	private static final int DEBUG = 3;// tests systems
 	private static final int TEST_MOVE = 4;// perform set motions
+	private static final int TEST_MOVE_2 = 5;
+	private static final int DUMMY_MULTI = 6;
 	private static final int DUMMY = 8;// do basic setup and end
 	private static final int DUMMY2 = 10;
 	private static final int CMP_SONAR_NAV = 12;// Competition sonar nav
 	private static boolean run = true;
 	private static core me;
 	public static final String[] MOTOR_LAYOUT = {"FL","FR","BL","BR","L","R"};
-	public static final String VERSION_NUMBER = "1.0.2";
+	public static final String VERSION_NUMBER = "1.0.4";
 	public static int debug_lvl = 0;
 	private static Logger logger = Logger.getLogger(basic.class.getCanonicalName());
 
@@ -23,16 +31,15 @@ public class basic {
 		BasicConfigurator.configure();
 		if (args.length > 1) {
 			System.out.print("I should add that in");
+			parser.parse(args);
 		}
 		parser me = new parser();
-		me.start();
-
-		/*
-		 * Scanner in = new Scanner(System.in); while(run){
-		 * System.out.print("> "); parser.parse(in.nextLine()); }
-		 * /*if(core.parse(args)){ //start(); }
-		 */
-		// in.close();*/
+		try{
+			me.start();
+		}catch(Exception e){
+			logger.info("How did you manage to mess THAT up?"+e);
+			e.printStackTrace();
+		}
 	}
 
 	public static void shutdown() throws InterruptedException {
