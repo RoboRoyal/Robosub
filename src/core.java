10114d00755070d45801bce5bcd983e18b767051
Dakota <RoboRoyal@users.noreply.github.com>
2017-11-02 21:40:09 -0400

    Update core.java

--- src/core.java
+++ src/core.java
@@ -16,19 +16,20 @@ import org.apache.log4j.Logger;
 public class core implements Runnable {
 	private static Logger logger = Logger.getLogger(core.class.getCanonicalName());
 	static Thread t;
-	static boolean RUN = false;
+	static boolean RUN = false;//allowed
+	public static boolean running = false;//actually running
+	static boolean shutOnFinish = true;
 	private static int OVER = 4;
 	public static int mode = 10;
 	public static boolean INIT = false;
 	private static long MAX_TIME = 20000;// mili
-	public static boolean running = false;
 
 	static void wait_start(Integer integer) {
 		System.out.println();
 		System.out.println("Waiting...");
 		try {
 			Thread.sleep(integer);
-			;
+			debug.log("Waited " + integer + " now starting");
 		} catch (Exception e) {
 
 		}
@@ -37,6 +38,8 @@ public class core implements Runnable {
 	}
 
 	static void shutdown() throws InterruptedException {
+		if (basic.debug_lvl > 0)
+			debug.logWithStack("System receaved shutdown command, shutting down");
 		logger.info("Ending...");
 		RUN = false;
 		running = false;
@@ -51,6 +54,7 @@ public class core implements Runnable {
 		Thread.sleep(200);
 		update.stop();
 		Thread.sleep(400);
+		//t = null;
 	}
 
 	/**
@@ -65,13 +69,21 @@ public class core implements Runnable {
 		logger.info("Starting mode: " + mode);
 		final long end_time = System.currentTimeMillis() + MAX_TIME;
 		boolean goal = false;
+		boolean status = status();
 		int step = 0;
 		int state = 0;
-		while (!goal && ((mode > 10) || (status() && System.currentTimeMillis() < end_time))) {
+		while (!goal && ((mode >= 10) || (status && System.currentTimeMillis() < end_time))) {
 			step++;
 			if (mode < 0)
 				debug.error("Invalid mode");
 			switch (mode) {
+			case (10):
+				// do nothing for debug
+				break;
+			case (0):
+				if(step == 20)
+					goal = true;
+			break;
 			case (3):
 				System.out.println("Running");
 				break;
@@ -105,30 +117,45 @@ public class core implements Runnable {
 			}
 			Thread.sleep(90);
 			if (basic.debug_lvl > 5 && step % 10 == 0) {
-				parser.log("State: " + step);
+				debug.log("Step: " + step);
 			}
+			status = status();
 		}
-		if (!status()) {
+		running = false;//need
+		if (!status) {
 			logger.error("Status failed");
+			debug.log("Status failed");
 			abort();
 		} else if (System.currentTimeMillis() >= end_time) {
 			logger.error("Time expired");
+			debug.log_err("Time expired");
 			abort();
 		} else if (goal) {
-			logger.info("Succsess! Ending prog");
-			movable.stop();
-			movable.surface();
-			basic.shutdown();
+			logger.info("Succsess! Ending running");
+			debug.log("Succsess! Ending running [goal reached]");
+			if(shutOnFinish){
+				logger.info("Shutting down; shut on finish");
+				debug.log("Shutting down; shut on finish");
+				movable.stop();
+				movable.surface();
+				basic.shutdown();
+			}else{
+				running = false;//redudency lol
+			}
+			
 		} else {
 			logger.info("IDK what happened but program stopped");
+			debug.log_err("IDK what happened but program stopped");
 		}
+		//we are no logging running even if we have permission to
+		//we have to wait to be restarted
 		running = false;
 	}
 
 	private static boolean test2(int step) {
 		boolean goal = false;
 		if (step == 10) {
-			movable.set_raw_input(2,2);
+			movable.set_raw_input(2, 2);
 			System.out.println(movable.print());
 		}
 		if (step == 20) {
@@ -150,6 +177,7 @@ public class core implements Runnable {
 
 	public static void abort() {
 		logger.error("Aborting!");
+		debug.log("Aborting!");
 		movable.abort();
 		try {
 			movable.abort();
@@ -158,9 +186,9 @@ public class core implements Runnable {
 			logger.info("Error 76");
 			e.printStackTrace();
 		}
-		try{
+		try {
 			Exception e = new Exception("Print me");
-		}catch(Exception e){
+		} catch (Exception e) {
 			logger.error("Stack:");
 			e.printStackTrace();
 		}
@@ -216,14 +244,14 @@ public class core implements Runnable {
 		movable.set_depth(5.0);
 		Thread.sleep(100);
 		if (state < 90) {
-			movable.move(0, 55);
+			movable.move();
 		}
 		if (state == 90) {
-			movable.face(90);
+			movable.face_R(90);
 			logger.info("Turning");
 		}
 		if (state > 110 && state < 200) {
-			movable.move(0, 55);
+			movable.moveInDir_R(90);
 		}
 		if (state == 200) {
 			return true;
@@ -245,11 +273,19 @@ public class core implements Runnable {
 	private static boolean status() {// check to make sure everything is OK
 		if (update.getWaterSensor() > .5) {
 			logger.error("Takeing on water; level at: " + update.getWaterSensor());
+			/*if (basic.logger_lvl > 0)
+				debug.log("Takeing on water; level at: " + update.getWaterSensor());*/
+			debug.log_err("Takeing on water; level at: " + update.getWaterSensor());
 			return false;// we are taking on water, abort!
 		}
 		if (Math.abs(update.IMU_pitch()) > 20 || Math.abs(update.IMU_roll()) > 20) {
-			logger.error("Pitch or roll to great, shutting down for safty;");
+			logger.error("Pitch or roll too great, shutting down for safty;");
 			logger.error("pitch: " + update.IMU_pitch() + " roll: " + update.IMU_roll());
+			if (basic.logger_lvl > 0)
+				debug.log("pithc/roll too great;");
+			if (basic.logger_lvl > 0)
+				debug.log("pitch: " + update.IMU_pitch() + " roll: " + update.IMU_roll());
+			debug.log_err("Bad stable: pitch: " + update.IMU_pitch() + " roll: " + update.IMU_roll());
 			return false;
 		}
 		// check anything else, tmp, battery level etc.
@@ -272,8 +308,9 @@ public class core implements Runnable {
 		if (!check(4)) {
 			return;
 		}
+		if (basic.logger_lvl > 0)
+			debug.log("----Initiating system----");
 		logger.info("----Initiating system----");
-		running = true;
 		Thread.sleep(300);// wait
 		update m4 = new update();
 		update.setUp();
@@ -296,6 +333,9 @@ public class core implements Runnable {
 		Thread.sleep(600);
 		INIT = true;
 		logger.info("----System sucsessfully initiated----");
+		if (basic.logger_lvl > 0)
+			debug.log("Init sucsess");
+
 	}
 
 	public static boolean check(int over) throws InterruptedException {
@@ -360,36 +400,55 @@ public class core implements Runnable {
 				System.out.println("No con; GC");
 				System.gc();
 			}
-			try{System.setProperty("no_fill", "true");
-			}catch(Exception e){
-				logger.error("I told you this was bad"); 
+			try {
+				System.setProperty("no_fill", "true");
+			} catch (Exception e) {
+				logger.error("I told you this was bad");
 				abort();
 			}
 		}
 	}
 
+	/**
+	 * Checks if no_fill is set
+	 * 
+	 * @return
+	 */
 	public static boolean no_fill() {
 		return (OVER == 9);
 	}
 
+	/**
+	 * Sets the maximum running time for the program. Doesn't apply to modes >=
+	 * 10.
+	 * 
+	 * @param value
+	 */
 	public static void setMaxTime(Integer value) {
 		MAX_TIME = value;
 	}
 
-	public static boolean selfTest() throws InterruptedException{
-		if(!INIT){
+	/**
+	 * Runs a self test on the sub checking major functionality. This includes
+	 * IMU data, motor control, and other tests. Parser command: test
+	 * 
+	 * @return
+	 * @throws InterruptedException
+	 */
+	public static boolean selfTest() throws InterruptedException {
+		if (!INIT) {
 			logger.info("Ya have to INIT and be running before self test ya dingis");
 			return false;
 		}
-		if(!RUN){
+		if (!RUN) {
 			logger.error("***You must start excecution before test.***");
 			logger.info("***Try setting mode to 51 with -m 51. Then start, then test***");
 			return false;
 		}
 		boolean allGood = true;
-		if(update.self_test()){
+		if (update.self_test()) {
 			logger.info("***Good connection***");
-		}else{
+		} else {
 			logger.error("***Bad connection***");
 			allGood = false;
 		}
@@ -400,69 +459,83 @@ public class core implements Runnable {
 		logger.info("***Place sub on level ground. Test will resume in 5 secons***");
 		Thread.sleep(5000);
 		logger.info("***Starting next test now***");
-		if(update.IMU_pitch > 2 || update.IMU_roll > 2){
-			logger.error("***Sub is not reading level; Roll: "+update.IMU_roll+" Pitch: "+update.IMU_pitch+"***");
+		if (update.IMU_pitch > 2 || update.IMU_roll > 2) {
+			logger.error(
+					"***Sub is not reading level; Roll: " + update.IMU_roll + " Pitch: " + update.IMU_pitch + "***");
 			logger.info("***IMU data is wrong or sub is not level. Please check***");
 			allGood = false;
-		}else{
+		} else {
 			logger.info("***Data looks good!***");
-			logger.info("***Exact results are: Roll: "+update.IMU_roll+" Pitch: "+update.IMU_pitch+"***");
+			logger.info("***Exact results are: Roll: " + update.IMU_roll + " Pitch: " + update.IMU_pitch + "***");
 		}
 		logger.info("***Now starting tilt test***");
 		logger.info("***Tilt sub aprox 30 degrees. Test will resume in 5 secons***");
 		Thread.sleep(5000);
-		if(Math.abs(update.IMU_pitch) < 20){
-			logger.error("***Sub is not reading tilt; Pitch: "+update.IMU_pitch+"***");
+		if (Math.abs(update.IMU_pitch) < 20) {
+			logger.error("***Sub is not reading tilt; Pitch: " + update.IMU_pitch + "***");
 			logger.info("***IMU data is wrong or sub is not tilted. Please check***");
 			allGood = false;
-		}else{
+		} else {
 			logger.info("***Data looks good!***");
-			logger.info("***Exact results are: Roll: "+update.IMU_roll+" Pitch: "+update.IMU_pitch+"***");
+			logger.info("***Exact results are: Roll: " + update.IMU_roll + " Pitch: " + update.IMU_pitch + "***");
 		}
 		logger.info("***Now starting roll test***");
 		logger.info("***Roll sub aprox 30 degrees. Test will resume in 5 secons***");
 		Thread.sleep(5000);
-		if(Math.abs(update.IMU_roll) < 20){
-			logger.error("***Sub is not reading tilt; Pitch: "+update.IMU_roll+"***");
+		if (Math.abs(update.IMU_roll) < 20) {
+			logger.error("***Sub is not reading tilt; Pitch: " + update.IMU_roll + "***");
 			logger.info("***IMU data is wrong or sub is not tilted. Please check***");
 			allGood = false;
-		}else{
+		} else {
 			logger.info("***Data looks good!***");
-			logger.info("***Exact results are: Roll: "+update.IMU_roll+" Pitch: "+update.IMU_pitch+"***");
+			logger.info("***Exact results are: Roll: " + update.IMU_roll + " Pitch: " + update.IMU_pitch + "***");
 		}
 		logger.info("***Now running extended connection test***");
 		int t;
 		int suc = 0;
-		for(t = 0; t<=25;t++){
-			if(update.self_test()) suc++;
+		for (t = 0; t <= 25; t++) {
+			if (update.self_test())
+				suc++;
 		}
-		if(suc!=t){
+		if (suc != t) {
 			allGood = false;
 			logger.error("***Sub did not pass extended connection test. Check connection.***");
-			logger.info("Exact ratio is: "+suc+" sucsesses out of "+t+" trials");
-		}else{
+			logger.info("Exact ratio is: " + suc + " sucsesses out of " + t + " trials");
+		} else {
 			logger.info("***Sub passed extended connection test. Check connection.***");
-			logger.info("***Exact ratio is: "+suc+" sucsesses out of "+t+" trials***");
+			logger.info("***Exact ratio is: " + suc + " sucsesses out of " + t + " trials***");
 		}
-		//TODO
-		//add in tests for sonar etc
-		if(allGood){
+		logger.info("Running status test");
+		Thread.sleep(500);
+		if (status()) {
+			logger.info("Passed status test");
+		} else {
+			logger.info("Failed status check");
+		}
+		// TODO
+		// add in tests for sonar etc
+		if (allGood) {
 			logger.info("***Sub passed all tests!***");
-		}else{
+		} else {
 			logger.info("***Sub didn't pass all tests. Please check it***");
 		}
 		return allGood;
 	}
+
+	public static boolean run_motors() {
+		return (System.getProperty("os.name").contains("ras"));
+	}
+
 	@Override
 	public void run() {
 		try {
+			running = true;
 			RUN = true;
 			runMode(mode);
 		} catch (InterruptedException e) {
 			debug.error(e.getLocalizedMessage());
 			e.printStackTrace();
 		}
-
 	}
 
 	public void start() {
@@ -472,10 +545,18 @@ public class core implements Runnable {
 				t = new Thread(this, "core");
 				t.start();
 			} else {
-				logger.error("big problems here");
+				logger.error("big problems here:"+t);
 			}
 		} else {
 			logger.error("Trying to make second instance of core");
+		}
+	}
+
+	public static void reset() {
+		t = null;
+		System.gc();
+		if (t == null) {
+			System.out.println("Didnt work");
 		}
 	}
 }
